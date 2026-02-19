c7e42e62150625873810cd5e241d4d5e78c49bdf
Abdurahim <ergashev20041712@gmail.com>
2026-02-20 03:08:48 +0500

    Yangi fayllar

---
+++ hello world.py
@@ -0,0 +1,53 @@
+#Name:Ergashev Abdurahim
+#Date:10.02.2026
+#Course:Python
+ism="Abdurahim"
+familiya="Ergashev"
+#print("Mening ismim"+ism)
+#print("Mening ismim"+' '+ism)
+#print(ism)
+#print(familiya)
+#print(ism+' '+familiya)
+#print("Hello\tworld")
+#print (ism+' '+familiya)
+#print ("Hello\tworld")
+#ism_sharif=f"{ism} {familiya}"
+#print(ism_sharif)
+#print(ism_sharif.upper())
+#ism_sharif=ism_sharif.title()
+#print(ism_sharif)
+#avtolar = ['audi' ,'bmw' ,'ford' ,'lexus']
+#for avto in avtolar:
+  #  if avto == 'bmw' :
+   #     print(avto.upper())
+   # else: 
+       # print(avto.title())
+#login = input('Yangi login kiriting:\n>>>')
+#if len(login)<=8:
+  # print ("Login 8 ta harfdan ko`p bo`lishi shart!")    
+#else:
+ #   print("Ko`d muvaffaqqiyatli amalga oshirildi")
+
+
+
+#yil = int (input("Tug`ilgan yilingizni kiriting!:\n>>>"))
+#if 2026-yil<18:
+  #  print(f"Yoshingiz {2026-yil} da ekan!!!")
+  #  print("Kirish mumkin emas!!!")
+#else:
+   # print('Xush kelibsiz!')
+yosh = int(input("Yoshingiz nechchida?\n>>>>"))
+if yosh<=4:
+  narx = 0
+elif yosh<=12:
+  narx = 5000
+elif yosh<=18:
+  narx = 10000
+else:
+  narx = 12000
+print(f"Sizga kirish {narx} so`m")
+
+
+   
+   
+   
